244d323a6b4789284d9bdc54a094ab876202d3ce
Christopher Varakliotis <c.varakliotis@gmail.com>
2017-02-13 11:17:46 +0200

    redux-form field level validation

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -2,23 +2,28 @@ import React, {Component} from 'react';
 import { Field, reduxForm } from 'redux-form';
 import Button from 'react-bootstrap/lib/Button';
 import { renderField, renderTextarea } from '../helpers';
-
-const validate = values => {
-	const errors = {};
-	if (!values.email) {
-		errors.email = 'Required';
-	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-		errors.email = 'Invalid email address';
-	}
-	return errors;
-};
+import { required, email, minLength } from '../validators';
 
 
 const LoginForm = ({ handleSubmit, pristine, reset, submitting }) => (
 	<form onSubmit={handleSubmit}>
-		<Field name="username" type="text" component={renderField} label="Username"/>
-		<Field name="email" type="text" component={renderField} label="Email"/>
-		<Field name="bio" component={renderTextarea} label="Bio"/>
+		<Field
+			name="username"
+			type="text"
+			component={renderField}
+			label="Username"
+			validate={[ required, email ]}/>
+		<Field
+			name="email"
+			type="text"
+			component={renderField}
+			label="Email"
+			validate={[ required, email ]} />
+		<Field
+			name="bio"
+			component={renderTextarea}
+			label="Bio"
+			validate={[ required, minLength(10) ]} />
 		<div>
 			<Button type="submit" bsStyle="primary" disabled={pristine || submitting}>Submit</Button>
 			<Button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</Button>
@@ -28,6 +33,5 @@ const LoginForm = ({ handleSubmit, pristine, reset, submitting }) => (
 
 
 export default reduxForm({
-	form: 'login',  // a unique identifier for this form
-	validate
+	form: 'login' // a unique identifier for this form
 })(LoginForm);
